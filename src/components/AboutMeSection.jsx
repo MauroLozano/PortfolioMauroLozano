23a2eb46b44291383e27855c6df800a778b8eeda
Mauro Lozano <mauroloz2006@gmail.com>
2025-12-15 15:28:30 -0300

    Added content and social links to the header

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -8,7 +8,7 @@ function AboutMeSection(){
             <h2 className={styles.section__title}>SOBRE MÍ</h2>
             <div className={styles['aboutme__content']}>
                 <div className={styles['aboutme__info--container']}>
-                    <p>Soy Mauro Lozano, un estudiante de Tecnicatura en Desarrollo de Software. Mi primer contacto con la programacion fue de pequeño. Pasaba horas enganchado frente a la pantalla intentando depurar mi codigo, pero curiosamente no estaba sufriendo, se sentía como un desafío</p><br></br>
+                    <p>Soy <strong>Mauro Lozano</strong>, un estudiante de Desarrollo de Software. Mi primer contacto con la programacion fue de pequeño. Entusiasmado por el desarrollo de videojuegos, pasaba horas enganchado frente a la pantalla intentando depurar mi codigo, pero curiosamente no estaba sufriendo, se sentía como un <strong>desafío</strong>.</p><br></br>
                     <p>A lo largo de mi formación académica, he desarrollado varios proyectos en equipo. Esta experiencia me ha brindado la habilidad de trabajar cómodamente en entornos colaborativos y transmitir mis ideas adecuadamente.</p>
                 </div>
                 <div className={styles['aboutme__profile--container']}>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
 import React, { forwardRef } from "react";
 import styles from '../css/Header.module.css'
+import gitHubIcon from '../assets/social/github-img(40x40).webp'
+import linkedinIcon from '../assets/social/ln-img(40x40).webp'
+import Tooltip from './Tooltips'
 import logo from '../assets/icons/logo.webp'
 
 const Header = forwardRef((props, ref) => {
@@ -39,9 +42,20 @@ const Header = forwardRef((props, ref) => {
                     </div>
                 </div>
             </div>
-            <div className={styles['header__content']}>
+            <div className={styles.header__content}>
                 <div className={styles.logo__style}><img src={logo} alt="Logo del Portfolio" className={styles.logo__img}></img></div>
-                <h1 className={styles.header__title}>MAURO LOZANO</h1>
+                <div className={styles.header__data}>
+                    <h1 className={styles.header__title}>MAURO LOZANO</h1>
+                    <h3 className={styles.header__subtitle}>Desarrollador de Software</h3>
+                    <div className={styles.header__social}>
+                        <Tooltip text='GitHub'>
+                            <a href="https://github.com/MauroLozano" className={styles['header__social--link']} target="_blank" rel="noopener noreferrer"><img src={gitHubIcon} alt="GitHub" className={styles['header__social--img']} /></a>
+                        </Tooltip>
+                        <Tooltip text='LinkedIn'>
+                            <a href="https://www.linkedin.com/in/mauro-lozano-meana-621347355/?trk=opento_sprofile_topcard" className={styles['header__social--link']} target="_blank"><img src={linkedinIcon} alt="LinkedIn" className={styles['header__social--img']} /></a>
+                        </Tooltip>
+                    </div>
+                </div>
             </div>
         </header>
     );
