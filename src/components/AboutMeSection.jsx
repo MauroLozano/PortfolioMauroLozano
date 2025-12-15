338218ef878aa5d9928a7f2d05788a3ee6a32c57
Mauro Lozano <mauroloz2006@gmail.com>
2025-12-15 09:46:15 -0300

    Added responsive design to About Me section. Fixed Cumulative Layout Shifts issues.

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -8,8 +8,7 @@ function AboutMeSection(){
             <h2 className={styles.section__title}>SOBRE MÍ</h2>
             <div className={styles['aboutme__content']}>
                 <div className={styles['aboutme__info--container']}>
-                    <h3>Mauro Lozano</h3>
-                    <p>Soy un estudiante de Tecnicatura en Desarrollo de Software. Mi primer contacto con la programacion fue de pequeño. Pasaba horas enganchado frente a la pantalla intentando depurar mi codigo, pero curiosamente no estaba sufriendo, se sentía como un desafío</p>
+                    <p>Soy Mauro Lozano, un estudiante de Tecnicatura en Desarrollo de Software. Mi primer contacto con la programacion fue de pequeño. Pasaba horas enganchado frente a la pantalla intentando depurar mi codigo, pero curiosamente no estaba sufriendo, se sentía como un desafío</p>
                     <p>A lo largo de mi formación académica, he desarrollado varios proyectos en equipo. Esta experiencia me ha brindado la habilidad de trabajar cómodamente en entornos colaborativos y transmitir mis ideas adecuadamente.</p>
                 </div>
                 <div className={styles['aboutme__profile--container']}>
