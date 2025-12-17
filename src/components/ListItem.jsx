7bdc1b6e72fcc4a747f18c5e7ad63a850e5643ca
Mauro Lozano <mauroloz2006@gmail.com>
2025-12-17 14:56:02 -0300

    Made list items animation not to calculate with scrollHeight to avoid Layout Thrashing

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -5,24 +5,19 @@ function ListItem({id, title, content}){
 
     //This calculates the height of the content to assign the maxHeight to the element. It is used to make the maxHeigth transition smooth independently of the content size.
     const contentRef = useRef(null)
-    
-    useEffect(()=>{
-        const contentElement = contentRef.current 
-        const contentHeight = contentElement.scrollHeight + 'px'
-        isOpen ? contentElement.style.maxHeight = contentHeight : contentElement.style.maxHeight = 0
-    },[isOpen])
     const toggleOpen = ()=>{
         setIsOpen(!isOpen)
     }
-    const contentClass = isOpen ? `${styles.item__content} ${styles['item__content--enabled']}` : `${styles.item__content} ${styles['item__content--disabled']}`;
+    const listItemClass = isOpen ? `${styles['list__item--open']}` : ``;
+    const contentClass = isOpen ? `${styles['item__content--enabled']}` : ``;
     const dotClass = isOpen ? styles['item__dot-enabled']: styles['item__dot-disabled'];
     return(
-        <li key={id} onClick={toggleOpen} className={styles.list__item}>
-            <div className={styles.item__header}>
+        <li key={id} onClick={toggleOpen} className={`${styles.list__item} ${listItemClass}`}>
+            <div className={`${styles.item__header}`}>
                 <div className={styles['item__dot-container']}><div className={`${dotClass} ${styles.item__dot}`}></div></div>
                 <div className={styles.item__title}>{title}</div>
             </div>
-            <div className={contentClass} ref={contentRef}>
+            <div className={`${styles.item__content} ${contentClass}`} ref={contentRef}>
                 <p>{content}</p>
             </div>
         </li>
