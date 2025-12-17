6f315920856a8c53b7614ecb659a37ced18ea95b
Mauro Lozano <mauroloz2006@gmail.com>
2025-12-17 12:09:15 -0300

    Implemented vite-plugin-html to be able to add a 'preload' attribute to the styles tag genereted for vite.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { createHtmlPlugin } from 'vite-plugin-html'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), 
+    createHtmlPlugin({
+      minify: true,
+      inject:{
+        tags:[
+          {
+            tag: 'link',
+            attrs: {
+              rel: 'preload',
+              href: '/assets/style.css',
+              as: 'style'
+            },
+            injectTo: 'head-prepend',
+          }
+        ],
+      },
+    })
+  ],
   build: {
     cssCodeSplit: false,
-    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'assets/style.css';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
   }
 })
